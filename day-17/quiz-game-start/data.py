5e4e27b0d7034592f10743cd369dbbb55a325bfd
habsul-rignyr <joshamy@gmail.com>
2024-12-01 21:22:48 +0800

    pull questions from open trivia database and correct json encoding with html unescape

--- day-17/quiz-game-start/data.py
+++ day-17/quiz-game-start/data.py
@@ -1,14 +1,18 @@
-question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+import requests
+import html
+
+api_url = "https://opentdb.com/api.php?amount=20&category=9&type=boolean"
+response = requests.get(api_url)
+if response.status_code == 200:
+    data = response.json()
+
+question_data = []
+
+for question in data["results"]:
+    question_text = html.unescape(question.get("question", "No question available"))
+    correct_answer = html.unescape(question.get("correct_answer", "No answer available"))
+    question_data.append({
+        "text": question_text,
+        "answer": correct_answer
+    })
+
